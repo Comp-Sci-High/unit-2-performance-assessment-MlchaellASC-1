3bea56ea749b5ee4906971d050626e76ef9a9794
MlchaellASC <142234035+MlchaellASC@users.noreply.github.com>
2025-11-20 14:41:22 -0500

    Implement API call to OpenAI for responses

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,24 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let url = "https://api.openai.com/v1/responses";
+async function getAnswer() {
+const options = {
+method: "POST",
+headers : {
+    Authorization: `Bearer ` + apiKey,
+   "Content-Type": "application/json"
+},
+body:JSON.stringify({
+    model: prompt ("What version do you want to use: "),
+    input:prompt ("what do you wanna ask ChatGPT: ")
+})
+
+}
+const response = await fetch(url, options);
+const data = await response.json();
+console.log(data.output[0].content[0].text);
+return data;
+}
+console.log("Welcome to Chatgpt")
+getAnswer();
