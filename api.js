bd3dda737755c157fb0c5d5cb1ee6f30f30e9121
MlchaellASC <142234035+MlchaellASC@users.noreply.github.com>
2025-11-20 14:40:16 -0500

    Implement card selection and image fetching
    Added user prompts and API call for Yu-Gi-Oh card images.

--- api.js
+++ api.js
@@ -2,6 +2,16 @@ const env = require('dotenv').config({ quiet: true });
 const prompt = require('prompt-sync')();
 const apiKey = process.env.apiKey2
 
-// run your code with node api.js
-// add your apiKey to .env if your API needs one, otherwise ignore
-// Follow the checklist on the Performance Assessment Google Doc
+console.log("Welcome to the Yu-Gi-Oh Chat bot")
+console.log("Pick a card Yu-Gi-Oh card id from the cards below")
+console.log("80181649: A Case for K9", "91231901: Cell Recombination", "89474727: Stardust Chronicle Spark Dragon", "35618486:Blue eyes white drago")
+const choice = prompt("Enter the card you want to know about");
+
+console.log(`Fetching ${choice} images`);
+const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${choice}`;
+async function fetchCard(){
+const response = await fetch(url);
+const data = await response.json();
+console.log(data.data[0].card_images[0].image_url);
+}
+fetchCard();
